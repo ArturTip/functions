87e6ce2461dc1ea1507b9af23841e302cf8db9a3
Nikiforov <rx992.ya.ru>
2025-05-04 16:19:22 +0700

    correction error

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -11,10 +11,10 @@ fun main() {
 fun commissn(transfer: Int, previous: Int = 0, typeCard: String = "Mir"): Int {
     val monthlyLimit = 600_000
     val dayLimit = 150_000
-    val minVisaCommissn = 350
+    val minVisaCommissn = 35
     val percentVisaCommissn = 0.0075
     val visaCommissn = max(minVisaCommissn, (transfer * percentVisaCommissn).toInt())
-    val freePayMastercard = 170_000
+    val freePayMastercard = 75_000
     val percentMastercardCommissn = 0.0006
     val masterCardCommissn = when {
         (previous + transfer) < freePayMastercard && (previous + transfer) > 0 -> 0
